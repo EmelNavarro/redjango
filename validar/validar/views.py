cb3048128a8c3b469f193291dad6ddd65ad4269c
Eduardo <emelguera@gmail.com>
2020-03-26 13:02:08 -0500

    first commit

---
+++ validar/validar/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+import re
+def saludo(request): #primera vista
+	return HttpResponse("introdusca la contraseña como url")
+
+def validar(request,contrasegna):
+	
+		xpresionregular=re.compile(r'(([A-Z]{1,1})([1-9]{3,3})([a-z]*)(\W{3,3}))')
+		if xpresionregular.match(contrasegna)is not None:
+			return HttpResponse("%s, es una contraseña valida" %contrasegna)
+		else:
+			return HttpResponse("%s, no es una contraseña correcta"%contrasegna)
